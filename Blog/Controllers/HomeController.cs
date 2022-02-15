f29d8e544c6042eeed947ff7742d8ff8112aa88f
Edward <gulyaev.ed.v@gmail.com>
2022-02-15 21:48:30 +0400

    Initial

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error: ", ex);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Work()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error: ", ex);
+            }
+            return View();
+        }
+        public async Task<IActionResult> Single()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error: ", ex);
+            }
+            return View();
+        }
+        public async Task<IActionResult> About()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error: ", ex);
+            }
+            return View();
+        }
+        public async Task<IActionResult> Contact()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error: ", ex);
+            }
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error: ", ex);
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
